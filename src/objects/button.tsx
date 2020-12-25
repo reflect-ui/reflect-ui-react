f4451361ecd1169a881d326ba8e1f905a4efeb3d
Junho Yeo <hanaro0704@gmail.com>
2020-12-25 15:48:57 +0900

    feat(objects/button): style button from variant

--- src/objects/button.tsx
+++ src/objects/button.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { css, cx } from '@emotion/css';
 
 export type TSmall = 'small' | 's' | 'sm';
 export type TMedium = 'medium' | 'm' | 'md';
@@ -19,13 +20,46 @@ export interface IButton {
   radius?: number;
 }
 
+const buttonVariantStyles: { [key in TButtonVariant]: string } = {
+  [ButtonVariants.filled]: css`
+    background-color: #1890ff;
+    border: 0;
+    color: white;
+  `,
+  [ButtonVariants.outline]: css`
+    background-color: transparent;
+    border: 2px solid #1890ff;
+    color: #1890ff;
+  `,
+  [ButtonVariants.ghost]: css`
+    background-color: transparent;
+    border: 0;
+    color: #1890ff;
+  `,
+};
+
 const Button: React.FC<IButton> = ({
   size = 'medium',
   variant = 'filled',
   radius = 8,
   children,
 }) => {
-  return <button>{children}</button>;
+  return (
+    <button
+      className={cx(
+        css`
+          padding: 12px 24px;
+          font-weight: bold;
+          font-size: 24px;
+          line-height: 1.5;
+          border-radius: ${radius}px;
+        `,
+        buttonVariantStyles[variant]
+      )}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
