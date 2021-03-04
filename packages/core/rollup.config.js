e40ce4c93ff537e1d3fc6264e0e3cb087e43e8be
GwonHeeJun <developer_gwon@naver.com>
2021-03-05 03:47:53 +0900

    modify : fix rollup, compile typescript error

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -2,9 +2,8 @@ import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import typescript from '@rollup/plugin-typescript';
 import packageJSON from './package.json';
-
+import typescript from 'rollup-plugin-typescript2';
 const input = './src/index.ts';
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
